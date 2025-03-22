1f00442598684b49e2c1f9bf6fd1a056bbb44ec8
EdwardBelt <75886512+EdwardBelt@users.noreply.github.com>
2025-03-22 04:26:17 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import { Toaster } from 'react-hot-toast';
+import { AuthProvider } from '@/context/AuthContext';
 
 const inter = Inter({ subsets: ['latin'] });
 
