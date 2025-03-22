565914b658df41c134249bdfc2be0e72c68a5845
EdwardBelt <75886512+EdwardBelt@users.noreply.github.com>
2025-03-22 04:08:59 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['localhost'],
+  },
+  // Add this to prevent static optimization issues
+  experimental: {
+    isrFlushToDisk: false
   }
 }
 
